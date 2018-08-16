fcb941fc676b52c70d8097fb3d646dcb0a005924
Radu Luncasu <radu.luncasu@gmail.com>
2018-08-16 03:11:37 +0300

    mirror fix

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -199,38 +199,21 @@ public:
   virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
                                   int x, int y,
                                   int *matrix_x, int *matrix_y) const {
-    // const int panel_height = matrix_height / parallel_;
-    // const int visible_width = (matrix_width / 64) * 32;
-    // const int slab_height = 2 * panel_height;   // one folded u-shape
-    // const int base_y = (y / slab_height) * panel_height;
-    // y %= slab_height;
-    // if (y < panel_height) {
-    //   x += matrix_width / 2;
-    // } else {
-    //   x = visible_width - x - 1;
-    //   y = slab_height - y - 1;
-    // }
     const int visible_height = 3 * matrix_height;
     const int visible_width = matrix_width / 3;
 
-    const int r1_x = visible_width;
-    const int r1_y = matrix_height;
-
-    const int r2_x = visible_width * 2;
-    const int r2_y = matrix_height * 2;
-
-    const int r3_x = visible_width * 3;
-    const int r3_y = matrix_height * 3;
+    // flip x
+    x = visible_width - x;
 
     // second row
-    if (y >= r1_y && y < 2*r1_y) {
-      y = y - r1_y;
-      x = 2*r1_x - x - 1;
-    } else if (y >= r2_y) { // third row
-      y = 3*r1_y -y - 1;
-      x = 2*r1_x + x;
+    if (y >= matrix_height && y < 2 * matrix_height) {
+      y = y - matrix_height;
+      x = 2 * visible_width - x - 1;
+    } else if (y >= 2*matrix_height) { // third row
+      y = 3 * matrix_height -y - 1;
+      x = 2 * visible_width + x;
     } else { // first row
-      y = r1_y - y - 1;
+      y = matrix_height - y - 1;
       x = x;
     }
     *matrix_x = x;
